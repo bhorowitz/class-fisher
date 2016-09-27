dfadafb5db38d95dbed75d7bfd0fd893acb4fff5
Ben Horowitz <horowitz.ben@gmail.com>
2016-09-27 12:45:24 -0700

    simple speed optimization in fisher for loop + additional comments

--- simple_fisher.py
+++ simple_fisher.py
@@ -40,22 +40,39 @@ def delta_Cl(l,C_l,f_sky=f_sky,b=beamsize,w=weight):
     dC = 2/(f_sky*(2*l+1))*((C_l + w**2*B**(-2))**2)
     return dC
 
+
 def simple_fisher(params,params_var = [0], p_change = 0.00005,f_sky=f_sky, weight=weight, beamsize=beamsize, verbose=True):
     size = len(params_var)
     if params_var == [0]:
-        params_var = params
+        params_var = params #if varying parameters unspecified, vary all given parameters (Note: Doesn't work with discrete parameters)
         size = len(params.keys())
-    C=run_class(params)
-    F = np.zeros([size,size])
+    C=run_class(params) # calculates value at test point
+    F = np.zeros([size,size]) #initialized fisher matrix
     for i1,j1 in enumerate(params_var):
+        V1 = dC_dx(params,j1,C,p_change = p_change)
         for i2,j2 in enumerate(params_var):
             sum_ol = 0
-            V1 = dC_dx(params,j1,C,p_change = p_change)
+            print j1, j2
             V2 = dC_dx(params,j2,C,p_change = p_change)
-            for l,X in enumerate(C):
-                V_deltaC = 1/delta_Cl(X[0],X[1],f_sky=f_sky,b=beamsize,w=weight)
-                #print X[0],V_deltaC, V1[l][1],V2[l][1]
-                sum_ol = sum_ol + V1[l][1]*V2[l][1]*V_deltaC
+            for l,model in enumerate(C):
+                for X in range(0,3):
+                    for Y in range(0,3):
+                        #Currently only TT power spectrum fisher model working properly.
+                        if (X ==0 and Y==0): #TT, TT
+                            V_deltaC = 1/delta_Cl(model[0],model[1],f_sky=f_sky,b=beamsize,w=weight)
+                        else if (X==0 and Y==1) or (X==1 and Y==0): #TT, EE
+                             V_deltaC = 0
+                        else if (X==0 and Y==2) or (X==2 and Y==0): #TT, TE
+                             V_deltaC = 0
+                        else if (X==1 and Y==1): #EE,EE
+                            V_deltaC = 0 #1/delta_Cl(model[0],model[1+X],w=56*10**(-9)/T_cmb)
+                        else if (X==1 and Y==2) or (X==2 and Y==1): #TE,EE
+                            V_deltaC = 0
+                        else if (X==2 and Y==2): #TE, TE
+                            V_deltaC = 0 #1/delta_Cl(model[0],model[1+X],w=0)
+                        else:
+                            raise NameError("Channel doesn't exist!")
+                        sum_ol = sum_ol + V1[l][1+X]*V2[l][1+Y]*V_deltaC
             if verbose:
                 print j1,j2, sum_ol
             F[i1,i2]=sum_ol
